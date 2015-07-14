d18b2c67487b79175f227701c8551a84497418f4
Frankie O'Rourke <frankie@orourkedd.com>
2015-07-14 10:50:47 -0700

    Commands working

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+var _ = require('lodash');
+
 module.exports = function (gulp, distName, config) {
-	config = config || {};
+	config = _.assign({
+		scriptLibs: [],
+		styleLibs: []
+	}, config);
 
 	require('./lib/scripts-browserify')(gulp, distName, config);
 	require('./lib/scripts-build')(gulp, distName, config);
 	require('./lib/scripts-compile')(gulp, distName, config);
 	require('./lib/scripts-libs')(gulp, distName, config);
+
+	require('./lib/styles-sass')(gulp, distName, config);
+	require('./lib/styles-build')(gulp, distName, config);
+	require('./lib/styles-compile')(gulp, distName, config);
+	require('./lib/styles-watch')(gulp, distName, config);
+
+	require('./lib/watch')(gulp, distName, config);
+	require('./lib/assets-compile')(gulp, distName, config);
 };
